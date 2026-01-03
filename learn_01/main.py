16935c367dd2e6ece8b658e36f15bb2bb7210910
randomseed42 <randomseed42@protonmail.com>
2026-01-03 22:25:37 +0800

    add animate_circles

--- learn_01/main.py
+++ learn_01/main.py
@@ -4,9 +4,6 @@ from matplotlib.animation import FuncAnimation
 
 
 class Epicycle:
-    def __init__(self):
-        pass
-
     @staticmethod
     def get_circle_points(
         center: complex = 0 + 0j,
@@ -82,7 +79,7 @@ class Epicycle:
             return circle, line, trace
 
         def update(frame):
-            theta = - 2 * np.pi / frames * frame
+            theta = -2 * np.pi / frames * frame
             line.set_data(*cls.get_arrow_points(center, radius, theta))
             trace_points.append(center + radius * np.exp(1j * theta))
             trace.set_data([p.real for p in trace_points], [p.imag for p in trace_points])
@@ -103,7 +100,77 @@ class Epicycle:
         # with open('animation.html', 'w') as f:
         #     f.write(ani.to_jshtml())
 
+    @classmethod
+    def animate_circles(
+        cls, radius: list[float], theta: list[float], speed: list[float], orig: complex = 0 + 0j, frames: int = 360
+    ):
+        print(f'{radius=}')
+        print(f'{theta=}')
+        print(f'{speed=}')
+        fig, ax = plt.subplots(figsize=(6, 6))
+        ax.set_aspect('equal')
+        ax.set_axis_off()
+        xlim = sum(radius) * 1.1
+        ylim = sum(radius) * 1.1
+        ax.set_xlim(-xlim, xlim)
+        ax.set_ylim(-ylim, ylim)
+        circles = []
+        lines = []
+        trace_points = []
+        (trace,) = ax.plot([], [], color='red', lw=1)
+
+        def init_func():
+            center = orig
+            t0 = 0
+            for r, t in zip(radius, theta, strict=True):
+                (circle,) = ax.plot([], [], color='#cccccc80', lw=2)
+                t0 += t
+                (line,) = ax.plot([], [], color='steelblue', lw=1)
+                circles.append(circle)
+                lines.append(line)
+                center += r * np.exp(1j * t0)
+            trace_points.clear()
+            trace_points.append(center)
+            trace.set_data([p.real for p in trace_points], [p.imag for p in trace_points])
+            return circles + lines + [trace]
+
+        def update(frame):
+            center = orig
+            t0 = 0
+            for i, (r, t, s) in enumerate(zip(radius, theta, speed, strict=True)):
+                circles[i].set_data(*cls.get_circle_points(center, r))
+                t0 += t + np.pi / 180 * s * frame
+                lines[i].set_data(*cls.get_arrow_points(center, r, t0))
+                center += r * np.exp(1j * t0)
+            # if len(trace_points) > 2 and np.abs(center - trace_points[0]) < 6.2e-15:
+            #     raise ValueError('Circle center is already in trace_points')
+            trace_points.append(center)
+            trace.set_data([p.real for p in trace_points], [p.imag for p in trace_points])
+            return circles + lines + [trace]
+
+        _ = FuncAnimation(
+            fig,
+            update,
+            frames=frames,
+            init_func=init_func,
+            interval=0.1,
+            blit=True,
+            repeat=False,
+        )
+        plt.tight_layout()
+        plt.show()
+        # plt.close(fig)
+        # with open('animation.html', 'w') as f:
+        #     f.write(ani.to_jshtml())
+
 
 if __name__ == '__main__':
     epicycle = Epicycle()
-    epicycle.animate_circle(center=0 + 0j, radius=2.0, speed=1, frames=500)
+    K = 3
+    epicycle.animate_circles(
+        radius=sorted(np.random.randint(1, 10, K), reverse=True),
+        theta=list(np.random.randint(1, 10, K) / 10),
+        speed=sorted(np.random.randint(-10, 10, K) / 10),
+        orig=0 + 0j,
+        frames=10000,
+    )
